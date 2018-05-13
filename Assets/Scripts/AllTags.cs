f952201f1eb1a8f731f9de1eae8fb7b5e5d836e4
Fabiel <fabiel016@gmail.com>
2018-05-12 23:41:43 -0500

    add scroll and select color, remove doeble audio listener

--- Assets/Scripts/AllTags.cs
+++ Assets/Scripts/AllTags.cs
@@ -1,3 +1,4 @@
 ﻿public static class AllTags {
     public static string TERRAIN = "Terrain";
+    public static string INPUT_MOUSE_SCROLL = "Mouse ScrollWheel";
 }
--- Assets/Scripts/ShotPlayer.cs
+++ Assets/Scripts/ShotPlayer.cs
@@ -4,23 +4,49 @@ using System.Collections;
 public class ShotPlayer : MonoBehaviour {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-    [Range(2f, 20f)] public float speed = 10;
+    [Range(2f, 40f)] public float speed = 20f;
     public Color[] colors;
+    private int indexColor = 0;
 
     void Update() {
         if (Input.GetMouseButtonDown(0)){
             Fire();
         }
+        if (Input.GetAxis(AllTags.INPUT_MOUSE_SCROLL) > 0f) {
+            colorForward();
+        } else if (Input.GetAxis(AllTags.INPUT_MOUSE_SCROLL) < 0f) {
+            colorBackward();
+        }
+    }
+
+    private void colorForward() {
+        indexColor++;
+        if(indexColor >= colors.Length) {
+            indexColor = 0;
+        }
+        updateColorUI();
+    }
+
+    private void colorBackward() {
+        indexColor--;
+        if (indexColor < 0) {
+            indexColor = colors.Length - 1;
+        }
+        updateColorUI();
     }
 
     private void Fire() {
-        int colorIndex = Random.Range(0, colors.Length);
-        bulletPrefab.GetComponent<BulletExplosion>().color = colors[colorIndex];
+        bulletPrefab.GetComponent<BulletExplosion>().color = colors[indexColor];
         var bullet = (GameObject)Instantiate(
             bulletPrefab,
             bulletSpawn.position,
             bulletSpawn.rotation);
         // Add velocity to the bullet
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * speed;
+    }
+
+    private void updateColorUI() {
+        //TODO update UI
+        Debug.Log(string.Concat("Index color: ", indexColor));
     }
 }
