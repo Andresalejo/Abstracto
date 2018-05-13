91be56ebaa0254b31820843782e52523f0257354
Fabiel <fabiel016@gmail.com>
2018-05-13 11:35:07 -0500

    add destroyAfter Create

---
+++ Assets/Scripts/DestroyAfterCreate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterCreate : MonoBehaviour {
+
+    public int waitTime = 5;
+
+	void Start () {
+        Destroy(gameObject, waitTime);
+	}
+}
