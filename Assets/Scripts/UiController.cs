50e456f45e3fd609b4e8de399c0925a610b08622
Andresalejo <Andres943007@hotmail.com>
2018-05-13 11:15:10 -0500

    RevisionesNp

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -7,17 +7,43 @@ public class UiController : MonoBehaviour {
 
     public Text playerPoints;
     public Text timerText;
+    public Button lifeBar;
+    private float x;
+    private float y;
+    // Use this for initialization
+    void Start () {
+        x = lifeBar.image.rectTransform.sizeDelta.x;
+        y = lifeBar.image.rectTransform.sizeDelta.y;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         UpdatePlayerPoints();
         UpdateTime();
+        UpdateLifeBar();
+
 	}
+
+
+    private void UpdateLifeBar()
+    {
+        if (lifeBar != null) {
+            int playerLife = GameArcadeController.Instance.playerLife;
+            lifeBar.image.rectTransform.sizeDelta = new Vector2((playerLife * x) / 100, y);
+            if(playerLife <= 0)
+            {
+                lifeBar.gameObject.SetActive(false);
+            } else
+            if(playerLife < 45) {
+                ColorBlock colorBlock = lifeBar.colors;
+                colorBlock.normalColor = Color.red;
+                lifeBar.colors = colorBlock;
+            }
+        }
+
+    }
+
 
     private void UpdatePlayerPoints() {
         if(playerPoints != null) {
